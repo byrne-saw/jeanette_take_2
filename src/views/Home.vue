f955660a30e9c8113933190d046f700e207bd9e7
scottasc <scottschulfer@gmail.com>
2019-01-03 20:27:57 -0600

    trying things

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="home">
+
+
+      <header>
+            <div class="hero-text-box">
+                <h1>Goodbye junk food.<br>Hello super healthy meals.</h1>
+                <a class="btn btn-full js--scroll-to-plans" href="#">I'm hungry</a>
+                <a class="btn btn-ghost js--scroll-to-start" href="#">Show me more</a>
+            </div>
+            
+        </header>
+
+
+
+
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+
+
+
+
+
 </template>
 
 <script>
